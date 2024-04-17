043fdb220c08e6dbf8cc85cf3eff25ad85040db2
HX-Tfd <oxotoxigon@gmail.com>
2024-04-17 18:08:10 +0200

    fix non-compiling compute features

--- src/city.py
+++ src/city.py
@@ -96,7 +96,6 @@ class City:
         self.positions_end = None   
         self.start_times = None
         self.end_times = None  
-        self.task_routes = None # n_tasks x (src_vertex_id, dest_vertex_id)  
         self.scenario_start_times = None # n_tasks x n_scenarios
         self.scenario_end_times = None   # n_tasks x n_scenarios
         self.scenario_delays_inter = None # n_scenarios x (n_districts x 24)
@@ -125,17 +124,25 @@ class City:
         # Returns the district number of the district at position (x, y)
         return int(y / self.height * self.n_districts_y) * self.n_districts_x + int(x / self.width * self.n_districts_x)
 
+    # TODO: add start and end tasks
     def sample_tasks(self, start_low: float, start_high: float, multiplier_low: float, multiplier_high: float):
         x_start = np.random.uniform(0, self.width, self.n_tasks)
         y_start = np.random.uniform(0, self.height, self.n_tasks)
         x_end = np.random.uniform(0, self.width, self.n_tasks)
         y_end = np.random.uniform(0, self.height, self.n_tasks)
-        positions_start = [(x, y) for (x, y) in zip(x_start, y_start)]
-        positions_end = [(x, y) for (x, y) in zip(x_end, y_end)]
+        city_center = (self.width/2, self.height/2)
+
+        positions_start = [(x, y) for (x, y) in zip(x_start, y_start)] + [city_center]*2
+        positions_end = [(x, y) for (x, y) in zip(x_end, y_end)] + [city_center]*2
         self.positions_start = positions_start
         self.positions_end = positions_end
-        start_times = np.random.uniform(start_low, start_high, self.n_tasks)
-        multipliers = np.random.uniform(multiplier_low, multiplier_high, self.n_tasks)
+
+        final_task_time = N_HOURS * 60.0 - 1
+        random_delay = np.random.lognormal(SCENARIO_START_ZERO_UNIFORM_LOW, 
+                                           SCENARIO_START_ZERO_UNIFORM_HI, 
+                                            2)
+        start_times = np.concatenate(([0.0], np.random.uniform(start_low, start_high, self.n_tasks), [final_task_time]))
+        multipliers = np.concatenate(([random_delay[0]], np.random.uniform(multiplier_low, multiplier_high, self.n_tasks), [random_delay[1]]))
         end_times = start_times + multipliers * np.array([self.distance(x1, y1, x2, y2) for (x1, y1), (x2, y2) in zip(positions_start, positions_end)])
         self.start_times = start_times
         self.end_times = end_times
@@ -182,8 +189,8 @@ class City:
                                                          SCENARIO_START_ZERO_UNIFORM_HI, 
                                                          self.n_scenarios)
         self.scenario_start_times = np.stack([np.repeat(self.start_times[t], self.n_scenarios) + scenario_start_random_delay
-                                              for t in range(self.n_tasks)])
-        self.scenario_end_times = np.zeros((self.n_tasks, self.n_scenarios))
+                                              for t in range(self.n_tasks+2)])
+        self.scenario_end_times = np.zeros((self.n_tasks+2, self.n_scenarios))
         self.scenario_delays_intra = np.zeros((self.n_scenarios, self.n_districts, N_HOURS))
         self.scenario_delays_inter = np.zeros((self.n_scenarios, N_HOURS))
 
@@ -193,7 +200,7 @@ class City:
             self.scenario_delays_intra[i, :, :] = congestion
             self.scenario_delays_inter[i, :] = inter_congestion
 
-            for j in range(self.n_tasks):
+            for j in range(1, self.n_tasks+1):
                 z1 = self.scenario_start_times[j, i]
                 start_district_delay = congestion[start_districts[j], self.get_hour(self.scenario_start_times[j, i])] 
                 z2 = z1 + start_district_delay
@@ -229,11 +236,9 @@ class City:
                 from_vertex=Vertex(str(origin_id)),
                 to_vertex=Vertex(str(end_task))
             ))
-            # self.task_routes.add(()) # TODO add task start and end vertex position idxs so that we can get it in the feature computation
 
             # there is an edge only if we can reach destination from origin before start of task
             for dest_id in range((origin_id + 1), self.n_tasks):
-                print(dest_id)
                 # travel time from task_i end position to task_j start position
                 end_pos_x, end_pos_y = self.positions_end[origin_id]
                 start_pos_x, start_pos_y = self.positions_start[dest_id]
@@ -278,8 +283,8 @@ class City:
         start_pos_x, start_pos_y = self.positions_start[new_task_id]
 
         travel_time = self.distance(end_pos_x, end_pos_y, start_pos_x, start_pos_y)
-        perturbed_end_times = self.scenario_end_times[old_task_id]
-        perturbed_start_times = self.scenario_start_times[new_task_id]
+        perturbed_end_times = self.scenario_end_times[old_task_id, :]
+        perturbed_start_times = self.scenario_start_times[new_task_id, :]
         return perturbed_start_times - (perturbed_end_times + travel_time)
         
 
@@ -341,9 +346,9 @@ class City:
         
     @staticmethod
     def get_hour(minutes: float) -> int:
-        assert minutes >= 0, f"Minutes must be positive, got {minutes}"
-        assert minutes < N_HOURS * 60, f"Minutes must be less than {N_HOURS * 60}, got {minutes}"
-        return int(minutes // 60)
+        # assert minutes >= 0, f"Minutes must be positive, got {minutes}"
+        # assert minutes <= N_HOURS * 60, f"Minutes must be less equal than {N_HOURS * 60}, got {minutes}"
+        return int((minutes%(N_HOURS * 60)) // 60)
         
 
 if __name__ == "__main__":
@@ -361,15 +366,16 @@ if __name__ == "__main__":
     print([t for t in np.mean(city.scenario_start_times, axis=1)]) 
     print([t for t in np.mean(city.scenario_end_times, axis=1)]) 
 
-    assert np.all([np.all(t_end > t_start) for (t_end, t_start) in zip(city.scenario_end_times, city.scenario_start_times)])
+    assert np.all([np.all(t_end >= t_start) for (t_end, t_start) in zip(city.scenario_end_times[:-1], city.scenario_start_times[:-1])])
 
     print("build graph")
     city.create_graph()
     print("build graph done")
     
     # TODO do not run this yet
-    # print("compute features")
-    # feats = city.compute_features()
-    # print("compute features done")
-    # print("features: ", feats)
+    print("compute features")
+    feats = city.compute_features()
+    print("compute features done")
+    print("features: ", feats)
+    print("features shape: ", feats.shape)
 
