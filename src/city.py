c1f0026e75a9f0da64208f64c39bfe2efbf7bac6
LeonardSchroeter <leonardschroeter@gmail.com>
2024-04-18 19:47:26 +0200

    Refactors and fixes

--- src/city.py
+++ src/city.py
@@ -6,12 +6,12 @@ from constants import *
 
 class Vertex:
     def __init__(self, name, **kwargs) -> None:
-        self.name = name
+        self.name = str(name)
 
 
 class Edge:
-    def __init__(self, name, from_vertex, to_vertex, **kwargs) -> None:
-        self.name = name
+    def __init__(self, from_vertex: Vertex, to_vertex: Vertex, **kwargs) -> None:
+        self.name = f"{from_vertex.name}->{to_vertex.name}"
         self.from_vertex = from_vertex
         self.to_vertex = to_vertex
 
@@ -96,8 +96,8 @@ class City:
         self.positions_end = None   
         self.start_times = None
         self.end_times = None  
-        self.scenario_start_times = None # n_tasks x n_scenarios
-        self.scenario_end_times = None   # n_tasks x n_scenarios
+        self.scenario_start_times = None # n_scenarios x n_tasks
+        self.scenario_end_times = None   # n_scenarios x n_tasks
         self.scenario_delays_inter = None # n_scenarios x (n_districts x 24)
         self.scenario_delays_intra = None # n_scenarios x (24)
         self.graph = SimpleDirectedGraph()
@@ -131,17 +131,15 @@ class City:
         y_end = np.random.uniform(0, self.height, self.n_tasks)
         city_center = (self.width/2, self.height/2)
 
-        positions_start = [(x, y) for (x, y) in zip(x_start, y_start)] + [city_center]*2
-        positions_end = [(x, y) for (x, y) in zip(x_end, y_end)] + [city_center]*2
+        positions_start = list(zip(x_start, y_start)) + [city_center] * 2
+        positions_end = list(zip(x_end, y_end)) + [city_center] * 2
         self.positions_start = positions_start
         self.positions_end = positions_end
 
         final_task_time = N_HOURS * 60.0 - 1
-        random_delay = np.random.lognormal(SCENARIO_START_ZERO_UNIFORM_LOW, 
-                                           SCENARIO_START_ZERO_UNIFORM_HI, 
-                                            2)
-        start_times = np.concatenate(([0.0], np.random.uniform(start_low, start_high, self.n_tasks), [final_task_time]))
-        multipliers = np.concatenate(([random_delay[0]], np.random.uniform(multiplier_low, multiplier_high, self.n_tasks), [random_delay[1]]))
+
+        start_times = np.concatenate((np.random.uniform(start_low, start_high, self.n_tasks), [0.0, final_task_time]))
+        multipliers = np.concatenate((np.random.uniform(multiplier_low, multiplier_high, self.n_tasks), np.zeros(2)))
         end_times = start_times + multipliers * np.array([self.distance(x1, y1, x2, y2) for (x1, y1), (x2, y2) in zip(positions_start, positions_end)])
         self.start_times = start_times
         self.end_times = end_times
@@ -184,56 +182,57 @@ class City:
         end_times = self.end_times
 
         # for every task, sample a delay for every scenario
+        # is 0 by default using low = -inf and high = 1
+        # TODO: should this be constant for all tasks? Also, shouldn't this be zero for the dummy tasks?
         scenario_start_random_delay = np.random.lognormal(SCENARIO_START_ZERO_UNIFORM_LOW, 
                                                          SCENARIO_START_ZERO_UNIFORM_HI, 
                                                          self.n_scenarios)
-        self.scenario_start_times = np.stack([np.repeat(self.start_times[t], self.n_scenarios) + scenario_start_random_delay
-                                              for t in range(self.n_tasks+2)])
-        self.scenario_end_times = np.zeros((self.n_tasks+2, self.n_scenarios))
+        self.scenario_start_times = np.tile(self.start_times, self.n_scenarios).reshape((self.n_scenarios, self.n_tasks+2)) \
+                                    + scenario_start_random_delay.reshape(-1, 1)
+
+        self.scenario_end_times = np.zeros((self.n_scenarios, self.n_tasks+2))
         self.scenario_delays_intra = np.zeros((self.n_scenarios, self.n_districts, N_HOURS))
         self.scenario_delays_inter = np.zeros((self.n_scenarios, N_HOURS))
 
         # sample congestion and inter-congestion for every scenario per task
-        for i in range(self.n_scenarios):
+        for scenario in range(self.n_scenarios):
             congestion, inter_congestion = self.sample_congestion()
-            self.scenario_delays_intra[i, :, :] = congestion
-            self.scenario_delays_inter[i, :] = inter_congestion
+            self.scenario_delays_intra[scenario, :, :] = congestion
+            self.scenario_delays_inter[scenario, :] = inter_congestion
 
-            for j in range(1, self.n_tasks+1):
-                z1 = self.scenario_start_times[j, i]
-                start_district_delay = congestion[start_districts[j], self.get_hour(self.scenario_start_times[j, i])] 
-                z2 = z1 + start_district_delay
-                z3 = z2 + end_times[i] - start_times[i] + inter_congestion[self.get_hour(z2)]
-                end_district_delay = congestion[end_districts[j], self.get_hour(z3)]
-                self.scenario_end_times[j, i] = z3 + end_district_delay
+            for task in range(self.n_tasks):
+                xi_1 = self.scenario_start_times[scenario, task]
+                start_district_delay = congestion[start_districts[task], self.get_hour(xi_1)] 
+                xi_2 = xi_1 + start_district_delay
+                xi_3 = xi_2 + end_times[task] - start_times[task] + inter_congestion[self.get_hour(xi_2)]
+                end_district_delay = congestion[end_districts[task], self.get_hour(xi_3)]
+                self.scenario_end_times[scenario, task] = xi_3 + end_district_delay
 
     # initiates a simple directed graph of the city 
     def create_graph(self):
         assert not(self.positions_start is None or self.positions_end is None), "cannot create graph with positions not computed"
         assert not(self.start_times is None or self.end_times is None), "cannot create graph with start and end times not computed"
 
-        n_verts = self.n_tasks + 2 # [starting_task, ...job_tasks, end_task]
-        starting_task = 0
-        end_task = n_verts - 1
-        job_tasks = range(1, self.n_tasks + 1)
+        n_verts = self.n_tasks + 2 # [...job_tasks, starting_task, end_task]
+        starting_task = self.n_tasks
+        end_task = self.n_tasks + 1
+        job_tasks = range(self.n_tasks)
         self.task_routes = []
 
         # init vertices
         for v in range(n_verts):
-            self.graph.add_vertex(Vertex(name=str(v)))
+            self.graph.add_vertex(Vertex(name=v))
 
         # build graph for each task
         for origin_id in job_tasks:
             # add every task to base
             self.graph.add_edge(Edge(
-                name=f"{starting_task}->{origin_id}",
-                from_vertex=Vertex(str(starting_task)),
-                to_vertex=Vertex(str(origin_id))
+                from_vertex=Vertex(starting_task),
+                to_vertex=Vertex(origin_id)
             ))
             self.graph.add_edge(Edge(
-                name=f"{origin_id}->{end_task}",
-                from_vertex=Vertex(str(origin_id)),
-                to_vertex=Vertex(str(end_task))
+                from_vertex=Vertex(origin_id),
+                to_vertex=Vertex(end_task)
             ))
 
             # there is an edge only if we can reach destination from origin before start of task
@@ -247,44 +246,38 @@ class City:
 
                 if origin_end_time + travel_time <= dest_begin_time:
                     self.graph.add_edge(Edge(
-                        name=f"{origin_id}->{dest_id}", 
-                        from_vertex=Vertex(str(origin_id)),
-                        to_vertex=Vertex(str(dest_id))
+                        from_vertex=Vertex(origin_id),
+                        to_vertex=Vertex(dest_id)
                     ))
 
 
-    def get_perturbed_travel_time(self, from_node_id: str, to_node_id: str, scenario: int):
+    def get_perturbed_travel_time(self, from_node_id: int, to_node_id: int, scenario: int):
         # assumes that the node names are directly convertible to ints
-        old_task_id = int(from_node_id)
-        new_task_id = int(to_node_id)
-        end_pos_x, end_pos_y = self.positions_end[old_task_id]
-        start_pos_x, start_pos_y = self.positions_start[new_task_id]
+        end_pos_x, end_pos_y = self.positions_end[from_node_id]
+        start_pos_x, start_pos_y = self.positions_start[to_node_id]
         origin_district = self.get_district(end_pos_x, end_pos_y)
         dest_district = self.get_district(start_pos_x, start_pos_y)
 
-        z1 = self.scenario_end_times[old_task_id, scenario]
-        z2 = z1 + self.scenario_delays_intra[scenario, origin_district, self.get_hour(z1)] 
-        z3 = z2 + self.distance(end_pos_x, end_pos_y, start_pos_x, start_pos_y) \
-                + self.scenario_delays_inter[scenario, self.get_hour(z2)]
-        result = z3 + self.scenario_delays_intra[scenario, dest_district, self.get_hour(z3)]
+        xi_1 = self.scenario_end_times[scenario, from_node_id]
+        xi_2 = xi_1 + self.scenario_delays_intra[scenario, origin_district, self.get_hour(xi_1)] 
+        xi_3 = xi_2 + self.distance(end_pos_x, end_pos_y, start_pos_x, start_pos_y) \
+                + self.scenario_delays_inter[scenario, self.get_hour(xi_2)]
+        result = xi_3 + self.scenario_delays_intra[scenario, dest_district, self.get_hour(xi_3)] - xi_1
 
         return result
             
 
     # computes the slack in minutes for features
-    def compute_slacks_for_features(self, from_node_id: str, to_node_id: str) -> np.ndarray:
+    def compute_slacks_for_features(self, from_node_id: int, to_node_id: int) -> np.ndarray:
         # assumes that the node names are directly convertible to ints
         assert self.graph is not None , "cannot compute features with empty graph"
 
-        old_task_id = int(from_node_id)
-        new_task_id = int(to_node_id)
-        end_pos_x, end_pos_y = self.positions_end[old_task_id]
-        start_pos_x, start_pos_y = self.positions_start[new_task_id]
-
-        travel_time = self.distance(end_pos_x, end_pos_y, start_pos_x, start_pos_y)
-        perturbed_end_times = self.scenario_end_times[old_task_id, :]
-        perturbed_start_times = self.scenario_start_times[new_task_id, :]
-        return perturbed_start_times - (perturbed_end_times + travel_time)
+        perturbed_travel_times = np.zeros(self.n_scenarios)
+        for scenario in range(self.n_scenarios):
+            perturbed_travel_times[scenario] = self.get_perturbed_travel_time(from_node_id, to_node_id, scenario)
+        perturbed_end_times = self.scenario_end_times[:, from_node_id]
+        perturbed_start_times = self.scenario_start_times[:, to_node_id]
+        return perturbed_start_times - (perturbed_end_times + perturbed_travel_times)
         
 
 
@@ -296,7 +289,7 @@ class City:
         N = G.get_num_vertices()
         slack_list = np.array([
             [
-                (self.scenario_start_times[int(e.to_vertex.name), s] if int(e.to_vertex.name) < N else np.Inf) -
+                (self.scenario_start_times[s, int(e.to_vertex.name)] if int(e.to_vertex.name) < N else np.Inf) -
                 (self.end_times[int(e.from_vertex.name)] + self.get_perturbed_travel_time(int(e.from_node.name), int(e.to_node.name), s))
                 for s in range(self.n_scenarios)
             ] for e in E
@@ -311,7 +304,7 @@ class City:
         assert self.graph is not None , "cannot compute features with empty graph"
 
         n_feats = NUM_FEATURES
-        features = np.zeros((n_feats, self.graph.get_num_edges()))
+        features = np.zeros((self.graph.get_num_edges(), n_feats))
         cumul = CUMULATIVE_VALUES_SLACK_EVAL
 
         # features indices
@@ -321,17 +314,16 @@ class City:
         slack_cum_distr_idxs = range(11, n_feats)
 
         # we should be able to index the edges by id so that we can refer edge src/dest id to task id (getting start/end positions)
-        for (i, edge) in enumerate(list(self.graph.get_edges())):
+        for (i, edge) in enumerate(self.graph.get_edges()):
             # compute travel time 
-            print(len(self.positions_end))
             from_vertex_x, from_vertex_y = self.positions_end[int(edge.from_vertex.name)]
             to_vertex_x, to_vertex_y = self.positions_start[int(edge.to_vertex.name)]
-            features[travel_time_idx, i] = self.distance(from_vertex_x, from_vertex_y, to_vertex_x, to_vertex_y)
-            features[connected_to_src_idx, i] =  VEHICLE_COST if edge.from_vertex.name == 1 else 0.0
+            features[i, travel_time_idx] = self.distance(from_vertex_x, from_vertex_y, to_vertex_x, to_vertex_y)
+            features[i, connected_to_src_idx] =  VEHICLE_COST if edge.from_vertex.name == str(self.n_tasks) else 0.0
 
             slacks = self.compute_slacks_for_features(int(edge.from_vertex.name), int(edge.to_vertex.name))
-            features[slack_decile_idxs, i] = np.quantile(slacks, 0.1*np.array(range(9)))
-            features[slack_cum_distr_idxs, i] = [np.mean(slacks <= x) for x in cumul]
+            features[i, slack_decile_idxs] = np.quantile(slacks, 0.1*np.array(range(9)))
+            features[i, slack_cum_distr_idxs] = [np.mean(slacks <= x) for x in cumul]
 
         return features
     
@@ -347,7 +339,7 @@ class City:
     def get_hour(minutes: float) -> int:
         # assert minutes >= 0, f"Minutes must be positive, got {minutes}"
         # assert minutes <= N_HOURS * 60, f"Minutes must be less equal than {N_HOURS * 60}, got {minutes}"
-        return int((minutes%(N_HOURS * 60)) // 60)
+        return int(minutes) // 60 % N_HOURS
         
 
 if __name__ == "__main__":
@@ -362,10 +354,11 @@ if __name__ == "__main__":
     print(city.end_times)
 
     print("mean of perturbed start and end times (in minutes): ")
-    print([t for t in np.mean(city.scenario_start_times, axis=1)]) 
-    print([t for t in np.mean(city.scenario_end_times, axis=1)]) 
+    print([t for t in np.mean(city.scenario_start_times, axis=0)]) 
+    print([t for t in np.mean(city.scenario_end_times, axis=0)]) 
 
-    assert np.all([np.all(t_end >= t_start) for (t_end, t_start) in zip(city.scenario_end_times[:-1], city.scenario_start_times[:-1])])
+    print(city.scenario_start_times <= city.scenario_end_times)
+    assert np.all((city.scenario_start_times <= city.scenario_end_times)[:,:-1]), "start times must be less than end times"
 
     print("build graph")
     city.create_graph()
