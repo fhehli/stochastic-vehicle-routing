3f11b0a3ecb45425f8ae04dc473c4366f0d5034c
Tobias R <rtobias286@gmail.com>
2024-04-19 23:48:05 +0200

    Update VSPSolver to work with city.py

--- src/VSPSolver.py
+++ src/VSPSolver.py
@@ -1,39 +1,42 @@
-import numpy as np
-
 # Gurobi Solver
 import gurobipy as gp
 from gurobipy import GRB
 from gurobipy import quicksum
 
+
 class VSPSolver:
-    def __init__(self, nodes, graph, jobs):
+    def __init__(self, city):
+        # Store the city
+        self.city = city
+
+        # Create the Gurobi Solver instance
         self.model = gp.Model("VSP")
-        # Create variables
-        self.graph = {}
-        for i in range(nodes):
-            for j in range(nodes):
-                if graph.has_edge(i, j):
-                    self.graph[i, j] = self.model.addVar(vtype=GRB.BINARY)
+
+        # Create the linear variables for the Solver (one for each edge)
+        for edge in self.city.graph.get_edges():
+            self.model.addVar(vtype=GRB.BINARY, name=edge.name)
 
         # Add constraints
-        for i in jobs:
+        for v in city.graph.get_vertices():
             # Flow Polytope
             self.model.addConstr(
-                quicksum(self.graph[j, i] for j in graph.predecessors(i)) == quicksum(self.graph[i, j] for j in graph.successors(i))
+                quicksum(self.model.getVarByName(e.name) for e in city.graph.get_outgoing_edges(v))
+                == quicksum(self.model.getVarByName(e.name) for e in city.graph.get_incoming_edges(v))
             )
             # Task Covering
             self.model.addConstr(
-                quicksum(self.graph[j, i] for j in graph.predecessors(i)) == 1
+                quicksum(self.model.getVarByName(e.name) for e in city.graph.get_outgoing_edges(v)) == 1
             )
+
+        # The model is updated lazily (https://support.gurobi.com/hc/en-us/articles/19459921918737-What-does-Awaiting-Model-Update-mean)
+        # Update manually
+        self.model.update()
 
     def solve(self, theta):
         # Set objective
-        self.model.setObjective(
-            theta.dot(self.paths),
-            GRB.MAXIMIZE
-        )
+        self.model.setObjective(theta.dot(self.model.getVars()), GRB.MAXIMIZE)
 
         # Optimize model
         self.model.optimize()
 
-        return self.model.getVars() # TODO: maybe need to extract values
+        return self.model.getVars()  # TODO: maybe need to extract values
