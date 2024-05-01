79bf142089c2db1b4ad7d85dd076be835ed863a5
Fabio Hehli <f4biohehli@hotmail.com>
2024-05-01 16:13:36 +0200

    fix VSPSolver
    each datapoint is a different city

--- src/VSPSolver.py
+++ src/VSPSolver.py
@@ -7,40 +7,34 @@ from gurobipy import quicksum
 # Appendix B.1.1 of https://arxiv.org/pdf/2207.13513.pdf
 # https://github.com/BatyLeo/StochasticVehicleScheduling.jl/blob/main/src/learning/easy_problem.jl
 class VSPSolver:
-    def __init__(self, city):
-        # Store the city
-        self.city = city
-
+    def solve(self, theta, graph):
         # Create the Gurobi Solver instance
-        self.model = gp.Model("VSP")
+        model = gp.Model("VSP")
 
         # Create the linear variables for the Solver (one for each edge)
-        for edge in self.city.graph.get_edges():
-            self.model.addVar(vtype=GRB.BINARY, name=edge.name)
+        for edge in graph.get_edges():
+            model.addVar(vtype=GRB.BINARY, name=edge.name)
 
         # The model is updated lazily (https://support.gurobi.com/hc/en-us/articles/19459921918737-What-does-Awaiting-Model-Update-mean)
-        self.model.update()
+        model.update()
 
         # Add constraints (we do not need theses constraints for the nodes `o` and `d`)
-        for v in list(self.city.graph.get_vertices())[:-2]:
+        for v in list(graph.get_vertices())[:-2]:
             # Flow Polytope
-            self.model.addConstr(
-                quicksum(self.model.getVarByName(e.name) for e in self.city.graph.get_incoming_edges(v))
-                == quicksum(self.model.getVarByName(e.name) for e in self.city.graph.get_outgoing_edges(v))
+            model.addConstr(
+                quicksum(model.getVarByName(e.name) for e in graph.get_incoming_edges(v))
+                == quicksum(model.getVarByName(e.name) for e in graph.get_outgoing_edges(v))
             )
             # Task Covering
-            self.model.addConstr(
-                quicksum(self.model.getVarByName(e.name) for e in self.city.graph.get_incoming_edges(v)) == 1
-            )
+            model.addConstr(quicksum(model.getVarByName(e.name) for e in graph.get_incoming_edges(v)) == 1)
 
         # Update manually
-        self.model.update()
+        model.update()
 
-    def solve(self, theta):
         # Set objective
-        self.model.setObjective(theta.dot(self.model.getVars()), GRB.MINIMIZE)
+        model.setObjective(theta.dot(model.getVars()), GRB.MINIMIZE)
 
         # Optimize model
-        self.model.optimize()
+        model.optimize()
 
-        return [var.X for var in self.model.getVars()]
+        return [var.X for var in model.getVars()]
